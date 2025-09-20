db54af24ccfe55a06ee09ef4d5b31a0fc21680e1
Premitha Pagadala <premithapagadala@gmail.com>
2025-09-20 11:39:54 -0400

    Assignment 02

--- Assignments/Assignment_02_Basic_Functions/Assignment_02.R
+++ Assignments/Assignment_02_Basic_Functions/Assignment_02.R
@@ -1,5 +1,8 @@
+# Define the vector of assignment scores
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+# Custom function to calculate the mean of a numeric vector
 myMean <- function(assignment2) {
   return(sum(assignment2) / length(assignment2))
 }
-myMean(assignment2)
+# Test the function on the vector
+myMean(assignment2) # Output: mean value of assignment2
